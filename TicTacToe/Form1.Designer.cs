ef90d2aabcdf2acac132e0b816af35474e862d92
MRLamb <lamb.matthew@gmail.com>
2016-09-29 21:00:29 -0500

    Initial Commit

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button23 = new System.Windows.Forms.Button();
+            this.button31 = new System.Windows.Forms.Button();
+            this.button32 = new System.Windows.Forms.Button();
+            this.button33 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radOgoes = new System.Windows.Forms.RadioButton();
+            this.radXgoes = new System.Windows.Forms.RadioButton();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(111, 92);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(77, 71);
+            this.button11.TabIndex = 0;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(194, 92);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(77, 71);
+            this.button12.TabIndex = 1;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(277, 92);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(77, 71);
+            this.button13.TabIndex = 2;
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button21
+            // 
+            this.button21.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button21.Location = new System.Drawing.Point(111, 169);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(77, 71);
+            this.button21.TabIndex = 3;
+            this.button21.UseVisualStyleBackColor = true;
+            // 
+            // button22
+            // 
+            this.button22.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button22.Location = new System.Drawing.Point(194, 169);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(77, 71);
+            this.button22.TabIndex = 4;
+            this.button22.UseVisualStyleBackColor = true;
+            // 
+            // button23
+            // 
+            this.button23.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button23.Location = new System.Drawing.Point(277, 169);
+            this.button23.Name = "button23";
+            this.button23.Size = new System.Drawing.Size(77, 71);
+            this.button23.TabIndex = 5;
+            this.button23.UseVisualStyleBackColor = true;
+            // 
+            // button31
+            // 
+            this.button31.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button31.Location = new System.Drawing.Point(111, 246);
+            this.button31.Name = "button31";
+            this.button31.Size = new System.Drawing.Size(77, 71);
+            this.button31.TabIndex = 6;
+            this.button31.UseVisualStyleBackColor = true;
+            // 
+            // button32
+            // 
+            this.button32.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button32.Location = new System.Drawing.Point(194, 246);
+            this.button32.Name = "button32";
+            this.button32.Size = new System.Drawing.Size(77, 71);
+            this.button32.TabIndex = 7;
+            this.button32.Tag = "command";
+            this.button32.UseVisualStyleBackColor = true;
+            // 
+            // button33
+            // 
+            this.button33.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button33.Location = new System.Drawing.Point(277, 246);
+            this.button33.Name = "button33";
+            this.button33.Size = new System.Drawing.Size(77, 71);
+            this.button33.TabIndex = 8;
+            this.button33.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radOgoes);
+            this.groupBox1.Controls.Add(this.radXgoes);
+            this.groupBox1.Location = new System.Drawing.Point(177, 345);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(115, 76);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Who goes next?";
+            // 
+            // radOgoes
+            // 
+            this.radOgoes.AutoSize = true;
+            this.radOgoes.Location = new System.Drawing.Point(21, 44);
+            this.radOgoes.Name = "radOgoes";
+            this.radOgoes.Size = new System.Drawing.Size(59, 17);
+            this.radOgoes.TabIndex = 1;
+            this.radOgoes.TabStop = true;
+            this.radOgoes.Text = "O goes";
+            this.radOgoes.UseVisualStyleBackColor = true;
+            // 
+            // radXgoes
+            // 
+            this.radXgoes.AutoSize = true;
+            this.radXgoes.Location = new System.Drawing.Point(21, 20);
+            this.radXgoes.Name = "radXgoes";
+            this.radXgoes.Size = new System.Drawing.Size(58, 17);
+            this.radXgoes.TabIndex = 0;
+            this.radXgoes.TabStop = true;
+            this.radXgoes.Text = "X goes";
+            this.radXgoes.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(193, 438);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(106, 30);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 25);
+            this.lblMessage.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 511);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button33);
+            this.Controls.Add(this.button32);
+            this.Controls.Add(this.button31);
+            this.Controls.Add(this.button23);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button button31;
+        private System.Windows.Forms.Button button32;
+        private System.Windows.Forms.Button button33;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radOgoes;
+        private System.Windows.Forms.RadioButton radXgoes;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        private int buttonsClicked = 0;
+        private bool gameOver = false;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (Control ctl in this.Controls)
+            {
+                //add event handlers to tic tac toe buttons
+                if (ctl is Button && ctl.Name != "btnReset")
+                    ctl.Click += button_Click;
+            }
+
+            DoReset();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (gameOver)
+                return;
+
+            ((Button)sender).Text = radXgoes.Checked ? "X" : "O";
+            this.btnReset.Focus();
+            ((Button)sender).Enabled = false;
+
+            radXgoes.Checked = !radXgoes.Checked;
+            radOgoes.Checked = !radXgoes.Checked;
+
+            string winner = CheckForWinner();
+            if (winner.Length > 0) 
+            {
+                this.lblMessage.Text = string.Format("We have a winner: {0}", winner);
+                gameOver = true;
+            }
+
+            buttonsClicked++;
+            if (buttonsClicked >= 9)
+            {
+                this.lblMessage.Text = "This game is a draw!";
+                gameOver = true;
+            }
+        }
+
+        private string CheckForWinner() 
+        {
+            string winner = string.Empty;
+
+            //TO DO:
+            //
+            //Check for a winner!
+            //
+            //Return either "X", "O", or "".
+
+            return winner;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            DoReset();
+        }
+
+        private void DoReset() 
+        {
+            foreach (Control ctl in this.Controls)
+            {
+                if (ctl is Button && ctl.Name != "btnReset") 
+                {
+                    ((Button)ctl).Text = string.Empty;
+                    ((Button)ctl).Enabled = true;
+                }
+            }
+
+            radXgoes.Checked = true;
+            buttonsClicked = 0;
+            gameOver = false;
+            this.lblMessage.Text = string.Empty;
+        }
+    }
+}
